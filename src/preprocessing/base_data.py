9fc0b338e9ca05fccc2be4eea563a282c3899614
Emilia Miąskiewicz <126571@student.uksw.edu.pl>
2026-03-11 18:40:08 +0100

    data: merged cleaned data into one file

--- src/preprocessing/base_data.py
+++ src/preprocessing/base_data.py
@@ -21,35 +21,61 @@ def load_csv(file_path: Path) -> pd.DataFrame:
     return df
 
 def clean_dataframe(df: pd.DataFrame) -> pd.DataFrame:
+    #drop ID and timestamp columns
     df = df.drop(columns=[c for c in DROP_COLUMNS if c in df.columns])
     print("Dropped ID and timestamp columns")
-    constant_columns = [col for col in df.columns if df[col].nunique() <= 1]
-    df = df.drop(columns=constant_columns)
-    print("Dropped columns with constant values")
+
+    #replace inf values with nan
     df = df.replace([np.inf, -np.inf], np.nan)
     print("Replaced inf and -inf with NaN")
+
+    # calculate missing values percentage
+    total_cells = df.shape[0] * df.shape[1]
+    total_missing = df.isna().sum().sum()
+    missing_percent = (total_missing / total_cells) * 100
+    print(f"Missing values: {total_missing} ({missing_percent:.4f}% of dataset)")
+
+    #drop rows with NaN
+    rows_before = df.shape[0]
     df = df.dropna()
-    print("Dropped NaN")
+    rows_after = df.shape[0]
+    removed_nan_rows = rows_before - rows_after
+    removed_nan_percent = (removed_nan_rows / rows_before) * 100
+    print(f"Removed rows with NaN: {removed_nan_rows} ({removed_nan_percent:.4f}% of rows)")
+
+    #drop duplicates
+    rows_before = df.shape[0]
     df = df.drop_duplicates()
-    print("Dropped duplicate rows")
+    rows_after = df.shape[0]
+    removed_duplicate_rows = rows_before - rows_after
+    duplicate_percent = (removed_duplicate_rows / rows_before) * 100
+    print(f"Removed duplicated rows: {removed_duplicate_rows} ({duplicate_percent:.4f}% of rows)")
+
     return df
 
-def process_file(file_path: Path):
+def process_file(file_path: Path, main_df: pd.DataFrame):
     print(f"Processing {file_path.name}")
-
     df = load_csv(file_path)
     df = clean_dataframe(df)
     print("Shape after cleaning: ", df.shape)
-    output_path = OUTPUT_DIR / file_path.name
-    df.to_csv(output_path, index=False)
-
-    print(f"Saved cleaned file to {output_path}")
+    #merge with main df
+    main_df = pd.concat([main_df,df], ignore_index=True)
+    return main_df
 
 def main():
     files = list(RAW_DATA_DIR.glob("*.csv"))
-
+    main_df = pd.DataFrame()
     for file in files:
-        process_file(file)
+        main_df = process_file(file, main_df)
+
+    constant_columns = [col for col in main_df.columns if main_df[col].nunique(dropna=False) <= 1]
+    main_df = main_df.drop(columns=constant_columns)
+    print(f"Dropped constant columns after merge: {constant_columns}")
+
+    output_path = OUTPUT_DIR / "cicids2017_preprocessed.csv"
+    main_df.to_csv(output_path, index=False)
+    print("Final dataset shape:", main_df.shape)
+    print(f"Saved merged dataset to {output_path}")
 
 if __name__ == "__main__":
     main()
