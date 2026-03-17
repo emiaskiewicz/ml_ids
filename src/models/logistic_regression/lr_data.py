6f04605dcf197793cfe015a67e8342ba88127f68
Emilia Miąskiewicz <126571@student.uksw.edu.pl>
2026-03-17 20:25:43 +0100

    feature: add initial logistic regression model and data processing files

---
+++ src/models/logistic_regression/lr_data.py
@@ -0,0 +1,4 @@
+from pathlib import Path
+
+def prepare_lr_data(config: dict) -> None:
+    pass
---
+++ src/models/logistic_regression/lr_model.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+import yaml
+from lr_data import prepare_lr_data
+
+BASE_DIR = Path(__file__).resolve().parents[3]
+CONFIG_PATH = BASE_DIR / "config" / "logistic_regression.yaml"
+
+def load_config(config_path: Path) -> dict:
+    with config_path.open("r", encoding="utf-8") as file:
+        return yaml.safe_load(file)
+
+def run_experiment() -> None:
+    config = load_config(CONFIG_PATH)
+    print(config)
+    prepare_lr_data(config)
+
+def main():
+    run_experiment()
+
+if __name__ == "__main__":
+    main()
